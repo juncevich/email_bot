f936df159eb1c78921e11b0c4c4467c8e757a64d
Yuntsevich Aleksandr Sergeevich <a.yuntsevich@tinkoff.ru>
2018-08-31 23:29:10 +0500

    Initial commit

---
+++ bot/src/main/java/com/example/bot/Bot.java
@@ -0,0 +1,27 @@
+package com.example.bot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+
+
+    @Override
+    public String getBotToken() {
+        return "639901087:AAHYvUKYqQWEHu69nxYCn_e4quEwBjdagkc";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println(update.getMessage().getText());
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "my_test_email_bot";
+    }
+}
---
+++ src/main/java/com/example/email_bot/EmailBotApplication.java
@@ -0,0 +1,12 @@
+package com.example.email_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailBotApplication.class, args);
+    }
+}
