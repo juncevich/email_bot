d273c98a8de31b0b9b37208c5d7bdf52694bc2d7
juncevich <a.juncevich@gmail.com>
2018-09-18 00:41:05 +0500

    Fix main test

--- src/test/java/com/example/postman/BotApplicationTests.java
+++ src/test/java/com/example/postman/BotApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.postman;
 
+import com.example.bot.BotApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = BotApplication.class)
 public class BotApplicationTests {
 
     @Test
