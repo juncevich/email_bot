a39c66f2790dd079bedfb540691d93dcd7b26d17
juncevich <a.juncevich@gmail.com>
2018-09-20 00:17:16 +0500

    Enable test. try to run in travis build

--- src/test/java/com/example/postman/BotApplicationTests.java
+++ src/test/java/com/example/postman/BotApplicationTests.java
@@ -1,7 +1,6 @@
 package com.example.postman;
 
 import com.example.bot.BotApplication;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = BotApplication.class)
 public class BotApplicationTests {
 
-    @Ignore
     @Test
     public void contextLoads() {
     }
